35904ada2fee087d9c2e1c4b0a72812dae6a689f
santh <santh@SANTHOSH>
2024-03-11 09:11:25 +0530

    oops

--- corejavaprograms_Mvit/src/Day2_oops/Executor.java
+++ corejavaprograms_Mvit/src/Day2_oops/Executor.java
@@ -11,6 +11,23 @@ public class Executor {
 	c1.setAddress("puducherry");
 	
 	System.out.println("customer id:"+c1.getcid()+" customer name:"+c1.getCname()+"customer Address:"+c1.getAddress());
-	}
+	
+	customer c2=new customer();
+	c2.setcid(112);
+	c2.setCname("sandy");
+	c2.setAddress("puducherry");
+	System.out.println(c2);//calling the tostring method
+	
+	customer c3=new customer();//invoke explict defalut constructor
+	System.out.println(c3);
+	customer c4=new customer(114,"diviya","chennai");//invoke parameterized constructor
+	System.out.print(c4);
+	
+
+	
+	
+	
+	
+}
 
 }
--- corejavaprograms_Mvit/src/Day2_oops/customer.java
+++ corejavaprograms_Mvit/src/Day2_oops/customer.java
@@ -5,10 +5,22 @@ public class customer {
 	private int cid;
 	private String cname;
 	private String address;
+	//default constructor
+	public customer() {
+		this.cid=02;
+		this.cname="unknown";
+		this.address="somewhere";
+	}
+	//parameterised constructor
+	public customer(int cid,String cname,String address){
+		this.cid=cid;
+		this.cname="cname";
+		this.address="adderss";
+		
+	}
 	
 	//getter and setter
-	public void setcid(int cid)
-	{
+	public void setcid(int cid){
 		this.cid=cid;
 		
 	}
@@ -33,6 +45,13 @@ public class customer {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
+	@Override
+	public String toString() {
+		return "customer [cid=" + cid + ", cname=" + cname + ", address=" + address + "]";
+	}
+	
+	
 	
 
 }
